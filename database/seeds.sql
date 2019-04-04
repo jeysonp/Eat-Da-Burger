b053180692539b21eb8e3258327e30c179066dcc
jeybone <jeysonp@gmail.com>
2019-04-04 17:09:19 -0500

    added most of the code from cats homework

---
+++ database/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Taco');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Frisco', true);
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
